9d28ed1e5b93fbc96cd9e3f01703ca0fb813788f
jkap <jbklego@gmail.com>
2010-07-26 16:45:49 -0400

    added the favicon, a skeleton for creating tapes, and changed how page titles are rendered

--- ttt.rb
+++ ttt.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require "sinatra/reloader" if development?
 require 'dm-core'
 require 'dm-validations'
 require 'dm-migrations'
@@ -28,15 +29,22 @@ end
 
 ## Controller Actions
 
-# index
+titlePrefix = 'tapestapestapes'
 
+# index
 get '/' do
 	@tapes=Tape.all :order=>[:created_at]
+	@pageTitle = titlePrefix
 	haml :index
 end
 
 # create tape
-post '/' do
+post '/new/tape' do
 	tape=Tape.create(:title=>params[:title],:created_at=>Time.now)
 	redirect '/'
+end
+
+get '/new/tape' do
+	@pageTitle = titlePrefix + ' - new tape'
+	haml :newTape
 end
